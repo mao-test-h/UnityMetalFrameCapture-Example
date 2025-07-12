daa3c188ff9b487a9b8ae790b81cbaa8c98f9f60
mao-test-h <miymao78.d@gmail.com>
2025-07-12 23:45:20 +0900

    refactor: 警告抑制のためにリネーム

--- Assets/_Development/Runtime/Misc/Rotation.cs
+++ Assets/_Development/Runtime/Misc/Rotation.cs
@@ -4,12 +4,12 @@ namespace MetalFrameCapture.Development
 {
     public sealed class Rotation : MonoBehaviour
     {
-        [Range(0f, 1f)] public float Intensity = 0.5f;
+        [Range(0f, 1f)] public float intensity = 0.5f;
 
         private void Update()
         {
-            var intensity = Intensity * 128f;
-            transform.Rotate(0, Time.deltaTime * intensity, 0);
+            var val = intensity * 128f;
+            transform.Rotate(0, Time.deltaTime * val, 0);
         }
     }
 }
